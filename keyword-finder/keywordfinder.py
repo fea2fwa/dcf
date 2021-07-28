9b5005e366275eac065fd0f11db853f295827acf
fea2fwa <fea2fwa@gmail.com>
2021-07-27 20:01:51 -0700

    Total info added

--- keyword-finder/keywordfinder.py
+++ keyword-finder/keywordfinder.py
@@ -1,6 +1,6 @@
 from stats import wordcloudcreation,frequentlyusedwords
 
-filenames = ["storage_urls-texts","poweredge_urls-texts","networking_urls-texts"]
+filenames = ["storage_urls-texts","poweredge_urls-texts","networking_urls-texts","total-texts"]
 
 for file in filenames:
     wordcloudcreation(file)
